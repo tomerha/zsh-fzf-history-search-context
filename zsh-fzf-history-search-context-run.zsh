196092ee1a93588d0343d3a00532ebccd36751a8
Tomer Harpaz <tomer.harpaz1@cellebrite.com>
2025-05-14 16:30:26 +0300

    added support for showing the full selected command in the preview pane

--- zsh-fzf-history-search-context-run.zsh
+++ zsh-fzf-history-search-context-run.zsh
@@ -14,11 +14,13 @@ if [ -n "$ZSH_FZF_HISTORY_SEARCH_CONTEXT_HIDE" ]; then
   FZF_ARGS="$FZF_ARGS --preview-window=hidden"
 fi
 
-PREVIEW_CMD='(fc -li $(($(echo {1} | sed "s/\*//") - ($FZF_PREVIEW_LINES / 2))) $(($(echo {1} | sed "s/\*//") - 1)) 2>/dev/null | sed -EH "s/^\s*\d+\*?\s*/  /";
-fc -li {1} {1} | sed -EH "s/^\s*\d+\*?\s*/> /";
-fc -li $(($(echo {1} | sed "s/\*//") + 1)) $(($(echo {1} | sed "s/\*//") + ($FZF_PREVIEW_LINES / 2) - 1)) 2>/dev/null | sed -EH "s/^\s*\d+\*?\s*/  /") |
-cut -c1-$FZF_PREVIEW_COLUMNS'
-PREVIEW_CMD="fc -R $TEMP_HISTFILE; $PREVIEW_CMD"
+REMOVE_NUMBERING_CMD='sed -EH "s/^\s*\d+\*?\s*//"'
+CUR_LINE_PREVIEW_CMD='fc -li {1} {1} | '"$REMOVE_NUMBERING_CMD"' | fold -$(($FZF_PREVIEW_COLUMNS - 2))'
+
+PREVIEW_CMD='fc -R '"$TEMP_HISTFILE"';
+fc -li $(($(echo {1} | sed "s/\*//") - ($FZF_PREVIEW_LINES / 2))) $(($(echo {1} | sed "s/\*//") - 1)) 2>/dev/null | '"$REMOVE_NUMBERING_CMD"' | cut -c1-$FZF_PREVIEW_COLUMNS;
+'"$CUR_LINE_PREVIEW_CMD"' | sed "s/^/> /";
+fc -li $(($(echo {1} | sed "s/\*//") + 1)) $(($(echo {1} | sed "s/\*//") + ($FZF_PREVIEW_LINES / 2) - $('"$CUR_LINE_PREVIEW_CMD"' | wc -l))) 2>/dev/null | '"$REMOVE_NUMBERING_CMD"' | cut -c1-$FZF_PREVIEW_COLUMNS;'
 
 BIND="tab:become($0 $1 '' {1})"
 
